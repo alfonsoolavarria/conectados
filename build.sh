4d580f98bc84a628ccda4e5cde7b65d1d44f9395
Alfonso Olavarria <alfonsojn15@gmail.com>
2026-08-18 12:20:12 -0400

    Fix: mover migrate al startCommand (DATABASE_URL solo disponible en runtime)

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --no-input
 python manage.py import_campers
 python manage.py create_accounts
+
+exec gunicorn conectados.wsgi:application
