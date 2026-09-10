7c39b278060c881a40e456539598d1af890b0ac4
Alfonso Olavarria <alfonsojn15@gmail.com>
2026-09-10 11:15:13 -0400

    Fix: agregar collectstatic al build.sh para asegurar archivos estaticos en produccion

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 python manage.py import_campers
 python manage.py create_accounts
